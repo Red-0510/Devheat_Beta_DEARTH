0a729404af98f985a76089b2effb1fd1b852f03a
Red-0510 <69681031+Red-0510@users.noreply.github.com>
2022-06-25 19:50:24 +0530

    Add files via upload
    created model for event and migrated it created media folder to store uploaded media content

--- devEvents/event/models.py
+++ devEvents/event/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    serial_no=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=100)
+    author=models.CharField(max_length=100)
+    start_time=models.DateTimeField(auto_now_add=True,blank=True)
+    end_time=models.DateTimeField(auto_now_add=True,blank=True)
+    description=models.TextField()
+    content=models.TextField()
+    pub_timestamp=models.DateTimeField(auto_now=True)
+    image=models.ImageField(upload_to="event/images")
+
+    def __str__(self):
+        return self.title
+
+
+
